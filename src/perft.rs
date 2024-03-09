048d21443401004b07fdc52eb27874cbf0725bf3
Alexandre Truppel <alex.truppel@gmail.com>
2024-03-09 19:12:16 +0100

    depth 6 and 7

--- src/perft.rs
+++ src/perft.rs
@@ -72,7 +72,15 @@ pub fn perft_suite() -> () {
     let data = [
         (
             "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-            vec![(1, 20), (2, 400), (3, 8902), (4, 197281), (5, 4865609)],
+            vec![
+                (1, 20),
+                (2, 400),
+                (3, 8902),
+                (4, 197281),
+                (5, 4865609),
+                (6, 1),
+                (7, 1),
+            ],
         ),
         (
             "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
